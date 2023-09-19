95502afc987998b7e38482f8876a19c8702ee3d6
awilkins2 <125494114+awilkins2@users.noreply.github.com>
2023-09-19 14:21:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,133 @@
+################################list###################################################
+list1 = [1,2,3,4,5,6,7,8,9,10]
+list2 = ["a","b","c","d","e","f","g","h","i","j"]
+
+# in list1 print 3.....8
+
+#print the last item in list1
+
+#print "c" ... "g" in list2
+
+#print apples ... cherry
+# add an item to the end of the fruits list
+
+#insert an item at the beginning of the fruits list
+
+#remove the first item from the fruits list
+
+
+#remove the last item from the fruits list
+
+#remove the item at index 2 from the fruits list
+
+#remove the item "kiwi" from the fruits list
+
+#remove the item "banana" from the fruits list
+
+
+
+
+# Lists Practice #1
+# Print the following list on the screen:
+# ["Python", "is", "easy", "to", "learn"]
+
+# Lists Practice #1
+# Create a list with 5 elements, inside the variable my_list. You can include strings, booleans, numbers, etc.
+
+# Lists Practice #2
+# Add the element "motorcycle" to the following list of means of transportation:
+# transportation_means = ["plane", "car", "ship", "bicycle"]
+# You must not modify the already supplied line of code, but must use the appropriate list method to add a new element.
+
+# Lists Practice #3
+# Use the pop() method to remove the third item from the following list called fruits, and store it in a variable called deleted_item. Use list methods without altering the line of code already supplied.
+# apple
+# banana
+# mango
+# cherry
+# watermelon
+
+
+
+
+#######################################Dictionaries###############################
+# Dictionaries Practice #1
+# Create a dictionary called fruits with the following key-value pairs:
+# apple --> red
+# banana --> yellow
+# mango --> green
+# cherry --> red
+# watermelon --> green
+# Display the dictionary on the screen.
+
+# Dictionaries Practice #1
+# Create a dictionary called my_dict that stores the following information about a person:
+# name: Karen
+# surname: Jurgens
+# age: 35
+# occupation: Journalist
+# The names of the keys and values must be equal to the ones indicated above.
+
+#   Dictionaries Practice #2
+# Use print to returns the second item of the list called points2, inside the following dictionary.
+# If the value 300 were to change in the future, the code should work the same to return the value at that same position. To do this, you must refer to the names of the keys and/or indexes as appropriate.
+# my_dict = {"values_1":{"v1":3,"v2":6},"points":{"points1":9,"points2":[10,300,15]}}
+# print(my_dict[]) #Use dictionary indices to extract the second item of points2
+
+# Dictionaries Practice #3
+# Update the information in our dictionary called my_dict (reassigning new values to the keys as appropriate), and add a new key called "country" (without a tilde). The new data is:
+# name: Karen
+# surname: Jurgens
+# age: 36
+# occupation: Editor
+# country: Colombia
+# to do this, you should not change the line of code already written, but update the values using dictionary methods.
+# my_dict = {"name":"Karen", "surname":"Jurgens", "age":35, "occupation":"Journalist"}
+
+################################Tuples##################################
+
+# Tuples Practice #1
+# Use a tuple method to count the number of times the value 2 appears in the following tuple, and display the result (integer) on the screen:
+
+# my_tuple = (1, 2, 3, 2, 3, 1, 3, 2, 3, 3, 3, 1, 3, 2, 2, 1, 3, 2)
+
+# Tuples Practice #2
+# Convert the following tuple to a list, and store it in a variable called my_list.
+
+# my_tuple = (1, 2, 3, 2, 3, 1, 3, 2)
+
+# Tuples Practice #3
+# Extract the elements of the following tuple into four variables: a, b, c, d
+
+# my_tuple = (1, 2, 3, 4)
+
+###############################SETS########################################
+# Sets Practice #1
+# Join the following sets into one, called my_set_3:
+# {1, 2, "three", "four"}
+# {"three", 4, 5}
+# my_set_1 = {1, 2, "three", "four"}
+
+# my_set_2 = {"three", 4, 5}
+
+# Sets Practice #2
+# Remove a random item from the following set, using set methods.
+# raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
+
+# Sets Practice #3
+# Add the name Gunther to the following set, using set methods:
+# raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
+
+###########################Booleans#######################################
+
+# Booleans Practice #1
+# Make a comparison that returns a boolean and store the result (True/False) in a variable called test
+
+# Booleans Practice #2
+# Check if 17834/34 is greater than 87*56 and print the boolean result to the screen using print()
+
+# Booleans Practice #3
+# Check if the square root of 25 is equal to 5 and display the result (boolean) on the screen using print()
+
+###############################Proceed to last slide#################################
+
